7a5c5cc6dd92db977e4978c8d8964da5b784b8d4
caijh <caiqizhe@gmail.com>
2021-01-02 14:02:46 +0800

    feat： 日期范围

---
+++ src/main/java/com/github/caijh/commons/util/DateRange.java
@@ -0,0 +1,24 @@
+package com.github.caijh.commons.util;
+
+import java.util.Date;
+
+public class DateRange {
+
+    private final Date start;
+
+    private final Date end;
+
+    public DateRange(Date start, Date end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+}
