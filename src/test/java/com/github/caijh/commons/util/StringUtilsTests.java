ad4ec8b85bb310a305c4a176a1fe368fe186fc8e
caijunhui <caiqizhe@gmail.com>
2021-01-14 16:55:01 +0800

    feat： Add test split

--- src/test/java/com/github/caijh/commons/util/StringUtilsTests.java
+++ src/test/java/com/github/caijh/commons/util/StringUtilsTests.java
@@ -3,6 +3,7 @@ package com.github.caijh.commons.util;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StringUtilsTests {
@@ -15,6 +16,13 @@ public class StringUtilsTests {
         System.out.println(0.0111111111111111111111111111111111D);
         System.out.println(StringUtils.toString(bigInteger));
         System.out.println(StringUtils.toString(bigDecimal));
+    }
+
+    @Test
+    public void testSplit() {
+        String str = "公安警察/治安警察";
+        String[] typeCodeNames = str.split("/");
+        Assert.assertEquals("治安警察", typeCodeNames[typeCodeNames.length - 1]);
     }
 
 }
