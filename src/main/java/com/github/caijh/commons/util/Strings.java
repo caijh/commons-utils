02c945df40e76a925233b5789da9776d66922015
caijunhui <caiqizhe@gmail.com>
2021-01-22 11:17:45 +0800

    feat： Strings add toListByComma method

--- src/main/java/com/github/caijh/commons/util/Strings.java
+++ src/main/java/com/github/caijh/commons/util/Strings.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.github.caijh.commons.util.constants.Delimiters;
 import org.apache.commons.lang3.StringUtils;
 
 public class Strings {
@@ -14,6 +15,10 @@ public class Strings {
 
     public static List<String> toList(String s, String regex) {
         return Arrays.stream(s.split(regex)).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+    }
+
+    public static List<String> toListByComma(String s) {
+        return toList(s, Delimiters.COMMA);
     }
 
 }
