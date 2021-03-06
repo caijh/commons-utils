df8b36aec13cbc1d654a903fdc23a1cd1fde314b
caijunhui <caiqizhe@gmail.com>
2021-03-06 15:08:46 +0800

    feat: 添加HexUtils

---
+++ src/main/kotlin/com/github/caijh/commons/util/HexUtils.kt
@@ -0,0 +1,30 @@
+package com.github.caijh.commons.util
+
+import okhttp3.internal.and
+
+object HexUtils {
+    fun byteArr2HexStr(buf: ByteArray): String {
+        val sb = StringBuilder()
+        for (i in buf.indices) {
+            var hex = Integer.toHexString(buf[i] and 0xFF)
+            if (hex.length == 1) {
+                hex = "0$hex"
+            }
+            sb.append(hex)
+        }
+        return sb.toString()
+    }
+
+    fun hexStr2ByteArr(hexStr: String): ByteArray? {
+        if (Strings.isBlank(hexStr)) {
+            return null
+        }
+        val result = ByteArray(hexStr.length shr 1)
+        for (i in result.indices) {
+            val index = i * 2
+            val j: Int = hexStr.substring(index, index + 2).toInt(16)
+            result[i] = j.toByte()
+        }
+        return result
+    }
+}
---
+++ src/test/kotlin/com/github/caijh/commons/util/HexUtilsTest.kt
@@ -0,0 +1,33 @@
+package com.github.caijh.commons.util
+
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class HexUtilsTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun byteArr2HexStr() {
+        val byteArr2HexStr = HexUtils.byteArr2HexStr("hello world".encodeToByteArray())
+        println(byteArr2HexStr)
+
+        val hexStr2ByteArr = HexUtils.hexStr2ByteArr(byteArr2HexStr)
+
+        Assert.assertEquals("hello world", hexStr2ByteArr?.let { String(it) })
+    }
+
+    @Test
+    fun hexStr2ByteArr() {
+
+    }
+
+}
