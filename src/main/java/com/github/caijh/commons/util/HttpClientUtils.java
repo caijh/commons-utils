a9e57705f89f10140d0e9faf97e66686b8812d78
caijunhui <caiqizhe@gmail.com>
2021-03-18 10:32:03 +0800

    refactor

--- src/main/java/com/github/caijh/commons/util/HttpClientUtils.java
+++ src/main/java/com/github/caijh/commons/util/HttpClientUtils.java
@@ -16,12 +16,18 @@ public class HttpClientUtils {
 
     private static final OkHttpClient httpClient = new OkHttpClient();
     private static final String APPLICATION_JSON_UTF8 = "application/json;charset=UTF-8";
+    private static final Headers HEADERS_NULL = null;
 
     private HttpClientUtils() {
     }
 
     public static String get(String url) {
-        return get(url, null);
+        return get(url, HEADERS_NULL);
+    }
+
+    public static <T> T get(String url, @Nonnull RespBodyConvertor<T> convertor) {
+        String respBody = get(url);
+        return convertor.convert(respBody);
     }
 
     /**
@@ -40,6 +46,35 @@ public class HttpClientUtils {
         return getRespBody(request);
     }
 
+    public static <T> T get(String url, @Nonnull Headers headers, @Nonnull RespBodyConvertor<T> convertor) {
+        String respBody = get(url, headers);
+        return convertor.convert(respBody);
+    }
+
+    public static String post(String url, String json) {
+        return post(url, null, MediaType.parse(APPLICATION_JSON_UTF8), json);
+    }
+
+    public static String post(String url, MediaType mediaType, String content) {
+        return post(url, null, mediaType, content);
+    }
+
+    public static String post(String url, Headers headers, MediaType mediaType, String content) {
+        Request.Builder builder = new Request.Builder();
+        if (headers != null) {
+            builder.headers(headers);
+        }
+        Request request = builder.url(url)
+                                 .post(RequestBody.create(content, mediaType))
+                                 .build();
+        return getRespBody(request);
+    }
+
+    public static <T> T post(String url, Headers headers, MediaType mediaType, String content, RespBodyConvertor<T> convertor) {
+        String respBody = post(url, headers, mediaType, content);
+        return convertor.convert(respBody);
+    }
+
     @Nonnull
     private static String getRespBody(Request request) {
         try {
@@ -52,21 +87,6 @@ public class HttpClientUtils {
         } catch (IOException e) {
             throw new HttpException(-1, e.getMessage());
         }
-    }
-
-    public static String post(String url, String json) {
-        return post(url, null, MediaType.parse(APPLICATION_JSON_UTF8), json);
-    }
-
-    public static String post(String url, Headers headers, MediaType mediaType, String content) {
-        Request.Builder builder = new Request.Builder();
-        if (headers != null) {
-            builder.headers(headers);
-        }
-        Request request = builder.url(url)
-                                 .post(RequestBody.create(content, mediaType))
-                                 .build();
-        return getRespBody(request);
     }
 
 }
