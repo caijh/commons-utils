26469f641aa1c51406fa3e109e1dd0ca5c13f62a
蔡俊辉 <caqizhe@gmail.com>
2021-04-24 15:10:15 +0800

    refactor: not throws exception

--- src/main/kotlin/com/github/caijh/commons/util/MD5.kt
+++ src/main/kotlin/com/github/caijh/commons/util/MD5.kt
@@ -2,7 +2,6 @@ package com.github.caijh.commons.util
 
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 object MD5 {
     /**
@@ -10,9 +9,7 @@ object MD5 {
      *
      * @param s 字符串
      * @return md5 string
-     * @throws NoSuchAlgorithmException if MD5 fail.
      */
-    @Throws(NoSuchAlgorithmException::class)
     fun md5(s: String): String {
         val md = MessageDigest.getInstance("MD5")
         md.update(s.toByteArray(StandardCharsets.UTF_8))
--- src/test/java/com/github/caijh/commons/util/Md5UtilsTest.java
+++ src/test/java/com/github/caijh/commons/util/Md5UtilsTest.java
@@ -1,14 +1,12 @@
 package com.github.caijh.commons.util;
 
-import java.security.NoSuchAlgorithmException;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class Md5UtilsTest {
 
     @Test
-    public void test_md5() throws NoSuchAlgorithmException {
+    public void test_md5() {
         String a = "123";
 
         Assert.assertEquals("202CB962AC59075B964B07152D234B70", MD5.INSTANCE.md5(a).toUpperCase());
