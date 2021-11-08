abf73390598fafcc83e409ed7462e2515002ab55
caijh <caiqizhe@gmail.com>
2021-11-08 14:48:40 +0800

    convert to kotlin

---
+++ src/main/kotlin/com/github/caijh/commons/util/constants/Delimiters.kt
@@ -0,0 +1,17 @@
+package com.github.caijh.commons.util.constants
+
+/**
+ * 分割符.
+ *
+ * @author caijunhui
+ * @since 2017/5/9
+ */
+object Delimiters {
+    const val COMMA = ","
+    const val AND = "&"
+    const val EQUAL = "="
+    const val DOT = "."
+    const val COLON = ":"
+    const val BAR = "|"
+    const val DASH = "-"
+}
