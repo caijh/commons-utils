54736ea4ba015256acbc2117788d2dcae31300a9
caijunhui <caiqizhe@gmail.com>
2021-11-11 08:48:44 +0800

    update Asserts with @JvmStatic

--- src/main/kotlin/com/github/caijh/commons/util/Asserts.kt
+++ src/main/kotlin/com/github/caijh/commons/util/Asserts.kt
@@ -4,16 +4,19 @@ import java.util.function.Supplier
 
 object Asserts {
 
+    @JvmStatic
     fun <T : Throwable> notNull(obj: Any?, supplier: Supplier<T>) {
         if (obj == null) {
             throw supplier.get()
         }
     }
 
+    @JvmStatic
     fun notNull(obj: Any?) {
         notNull(obj) { NullPointerException() }
     }
 
+    @JvmStatic
     fun <T : Throwable> isTrue(expression: Boolean, supplier: Supplier<T>) {
         if (!expression) {
             throw supplier.get()
