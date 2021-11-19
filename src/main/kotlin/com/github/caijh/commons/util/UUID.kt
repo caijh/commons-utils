61a95c671a18144260142cfe17a7cd51cf1e110a
蔡俊辉(10039330) <caijh@glodon.com>
2021-11-19 11:01:05 +0800

    feat: UUID add comment

--- src/main/kotlin/com/github/caijh/commons/util/UUID.kt
+++ src/main/kotlin/com/github/caijh/commons/util/UUID.kt
@@ -1,19 +1,41 @@
 package com.github.caijh.commons.util
 
+import java.util.*
 import java.util.UUID
 
+/**
+ * UUID工具类
+ */
 object UUID {
 
+    /**
+     * 获取UUID
+     *
+     * @return UUID
+     */
     @JvmStatic
     fun get(): String {
         return get(replaceDash = true, toUpperCase = true)
     }
 
+    /**
+     * 获取UUID
+     *
+     * @param replaceDash 是否替换dash为""
+     * @return UUID
+     */
     @JvmStatic
     fun get(replaceDash: Boolean): String {
         return get(true, replaceDash)
     }
 
+    /**
+     * 获取UUID
+     *
+     * @param replaceDash 是否替换dash为""
+     * @param toUpperCase 是否转换为大写
+     * @return UUID
+     */
     @JvmStatic
     fun get(replaceDash: Boolean, toUpperCase: Boolean): String {
         var uuid = UUID.randomUUID().toString()
@@ -21,7 +43,7 @@ object UUID {
             uuid = uuid.replace(Delimiters.DASH, "")
         }
         if (toUpperCase) {
-            uuid = uuid.toUpperCase()
+            uuid = uuid.uppercase(Locale.getDefault())
         }
         return uuid
     }
