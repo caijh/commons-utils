c056232f031a873bd4619ae357b438b62d84ab14
蔡俊辉(10039330) <caijh@glodon.com>
2022-05-26 16:07:34 +0800

    fix: tests

--- src/test/java/com/github/caijh/commons/util/UuidUtilsTest.java
+++ src/test/java/com/github/caijh/commons/util/UuidUtilsTest.java
@@ -3,6 +3,7 @@ package com.github.caijh.commons.util;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UuidUtilsTest {
 
@@ -10,7 +11,7 @@ public class UuidUtilsTest {
     public void uuid() {
         String uuid = UUID.get();
         System.out.println(uuid);
-        assertFalse(uuid.contains("-"));
+        assertTrue(uuid.contains("-"));
         uuid = UUID.get(true, true);
         assertFalse(uuid.contains("-"));
         System.out.println(uuid);
