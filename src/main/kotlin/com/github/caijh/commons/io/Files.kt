b058c81bbe62ef1cca4d6a078c161c2c2ba45dd6
johnhuitsai <caiqizhe@gmail.com>
2022-08-13 18:07:37 +0800

    Files

--- src/main/kotlin/com/github/caijh/commons/io/Files.kt
+++ src/main/kotlin/com/github/caijh/commons/io/Files.kt
@@ -1,6 +1,9 @@
 package com.github.caijh.commons.io
 
 import com.github.caijh.commons.util.Delimiters
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 object Files {
     @JvmStatic
@@ -9,5 +12,22 @@ object Files {
         return if (index < 0) {
             ""
         } else name.substring(index + 1)
+    }
+
+    @JvmStatic
+    fun write(inputStream: InputStream, outputStream: OutputStream) {
+        try {
+            BufferedInputStream(inputStream).use { bis ->
+                val buff = ByteArray(1024)
+                val os: OutputStream = outputStream
+                var i: Int
+                while (bis.read(buff).also { i = it } != -1) {
+                    os.write(buff, 0, i)
+                    os.flush()
+                }
+            }
+        } catch (e: Exception) {
+            throw e;
+        }
     }
 }
