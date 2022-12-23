cd041426125aac02ca91a64ef752fb749868b8f4
johnhuitsai <caiqizhe@gmail.com>
2022-12-23 23:39:14 +0800

    feat: Lists

---
+++ src/main/kotlin/com/github/caijh/commons/util/Lists.kt
@@ -0,0 +1,12 @@
+package com.github.caijh.commons.util
+
+/**
+ * List Utils.
+ */
+object Lists {
+
+    @JvmStatic
+    fun <T> newArrayList(): ArrayList<T> {
+        return arrayListOf();
+    }
+}
